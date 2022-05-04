2c8542ab762ebb40b817df1c2454ad0490bfa811
rg <garzon.r@gmail.com>
2022-05-04 02:20:50 -0400

    setup config stuff

--- app/routes.py
+++ app/routes.py
@@ -5,9 +5,4 @@ from app import app
 @app.route('/')
 @app.route('/index')
 def index():
-    conf = {
-        'twitter_url': 'http://twitter.com/gzgzgz____',
-        'linkedin_url': 'http://linkedin.com/in/garzonr',
-        'github_url': 'http://github.com/murdoc02'
-    }
-    return render_template('index.html',title='Home', conf=conf)
+    return render_template('index.html',title='Home', conf=app.config)
